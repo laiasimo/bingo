083d35ee89fdabd159f710050bf06f6878263e1a
Laia Simo Riudalbas <simoriud@SV-20M-010.local>
2016-07-11 16:57:11 +0200

    listing plans for this summer

---
+++ inst/topics/Plansforthissummer.R
@@ -0,0 +1,17 @@
+# Plans for this summer
+# Authors: tlaguna, laiasimo
+
+c("reading nice books", 
+  "attending CSAMA",
+  "sunbathing",
+  "writing applications for grants",
+  "assembling transcriptomes",
+  "partying",
+  "diving",
+  "gardening",
+  "trying new fancy restaurants",
+  "eating sausages and drinking beer",
+  "visiting vineyards",
+  "visit Japan",
+  "swimming"
+  )
